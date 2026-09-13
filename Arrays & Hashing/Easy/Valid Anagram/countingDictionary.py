3327180597e9c81b3c017c306c6824555776ab68
hoanghamhoc <nayuta1606@gmail.com>
2026-09-13 21:34:50 +0700

    Updates improvement on dictionary solves

--- Arrays & Hashing/Easy/Valid Anagram/countingDictionary.py
+++ Arrays & Hashing/Easy/Valid Anagram/countingDictionary.py
@@ -8,10 +8,12 @@ class Solution:
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i],0)
             countT[t[i]] = 1 + countT.get(t[i],0)
-        for c in countS:
+        """for c in countS:
             if countS[c] != countT[c]:
                 return False
-            return True
+            return True"""
+        ##improves by using dictionary comparison: the position of keys doesn't matter as long as they have the same number of keys and the keys match and their following values also match
+        return countS==countT
 
 
 o = Solution()
